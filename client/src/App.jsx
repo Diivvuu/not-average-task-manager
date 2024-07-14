182670d3c7331d08d08c5c88420990fea2c21ef7
Diivvuu <divyanshubawa9@gmail.com>
2024-07-15 00:20:07 +0530

    setting up frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="flex bg-indigo-300 min-h-screen">hello</div>;
+}
+
+export default App;
---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dashboard = () => {
+  return <div>dsahboard</div>;
+};
+
+export default Dashboard;
