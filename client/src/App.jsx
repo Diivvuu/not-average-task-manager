85276573814ae07c57045bef4dfb144af2facb1b
Diivvuu <divyanshubawa9@gmail.com>
2024-07-17 12:02:16 +0530

    client side routing, and login page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,56 @@
 import { useState } from "react";
+import Login from "./pages/Login";
+import { Navigate, Routes, Route, useLocation, Outlet } from "react-router-dom";
+import Tasks from "./pages/Tasks";
+import Users from "./pages/Users";
+import Trash from "./pages/Trash";
+import TaskDetails from "./pages/TaskDetails";
+import { Toaster } from "sonner";
+
+function Layout() {
+  const user = "";
+  const location = useLocation();
+
+  return user ? (
+    <div className="w-full h-screen flex flex-col md:flex-row">
+      <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
+        {/* <Sidebar /> */}
+      </div>
+      {/* <MobileSidebar/> */}
+      <div className="flex-1 overflow-y-auto">
+        {/* <Navbar/> */}
+        <div className="p-4 2xl:px-10">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="flex bg-indigo-300 min-h-screen">hello</div>;
+  return (
+    <main>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/completed/:status" element={<Tasks />} />
+          <Route path="/in-progress/:status" element={<Tasks />} />
+          <Route path="/todo/:status" element={<Tasks />} />
+          <Route path="/team" element={<Users />} />
+          <Route path="/trashed" element={<Trash />} />
+          <Route path="/task/:id" element={<TaskDetails />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
+      <Toaster richColors />
+    </main>
+  );
 }
 
 export default App;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,22 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
-  return <div>login</div>;
+  const user = "";
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
+  useEffect(() => {
+    user && navigate("/dashboard");
+  }, [user]);
+  return (
+    <div className="w-full min-h-screen flec items-center justify-center flex-col lg:flex-row">
+      login
+    </div>
+  );
 };
 
 export default Login;
